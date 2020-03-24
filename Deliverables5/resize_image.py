0a1d26a9310199f40a5a23cf2b1e7bc08a002d98
Alexandre <alexandre09@ua.pt>
2020-03-24 13:47:39 +0000

    more files

--- Deliverables5/resize_image.py
+++ Deliverables5/resize_image.py
@@ -4,16 +4,18 @@ import cv2 as cv
 
 def resize_images(directory, size):
 
-    i = 0
 
     for image in os.listdir(directory):
-        i = i+1
-        img = cv.imread(directory + image)
-        cv.imshow('img', img)
-        img = cv.resize(img, size, interpolation=cv.INTER_AREA)
-        cv.imwrite('../r_CAMBADA/' + str(i)+'.png', img)
+        if '.jpg' in image:
+            print("Found" + directory+image)
+            i = image.split('.')
+            img = cv.imread(directory + image)
+            cv.imshow('img', img)
+            img = cv.resize(img, size, interpolation=cv.INTER_AREA)
+            cv.imwrite('../workspace/training_demo/images/' + i[0]+'.jpg', img)
+            print("New file ../workspace/training_demo/images/" + i[0]+".jpg\n")
 
-resize_images('../CAMBADA/', (80,80))
-show_new_img('../r_CAMBADA/')
+resize_images('../workspace/training_demo/images/', (1280,720))
+
 
 
